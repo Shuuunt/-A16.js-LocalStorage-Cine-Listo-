d9fe420224a1567321169fd73e35dde790e5608d
Shuuunt <carlosevinah@gmail.com>
2026-03-13 22:10:07 +0100

    EJ2BASE

--- ACTIVIDAD16_Ej1/script.js
+++ ACTIVIDAD16_Ej1/script.js
@@ -47,59 +47,58 @@ function cuentaContraseña(password) {
 
 // Eventos de formulario
 form.addEventListener("submit", function(e) {
-e.preventDefault();
+    
+    e.preventDefault();
 
-// Usuario
-if(username.value === "") {
-    showError(username, "Se requiere introducir usuario");
-} else {
-    showSuccess(username); 
-}
+    // Usuario
+    if(username.value === "") {
+        showError(username, "Se requiere introducir usuario");
+    } else {
+        showSuccess(username); 
+    }
 
-// Edad
-if(Age.value === "") {
-    showError(Age, "Se requiere introducir Edad");
-}else if(Age.value < 0 || Age.value > 999) {
-    showError(Age, "La edad no puede ser menor a 0 o mayor a 999")
-} else {
-    showSuccess(Age); 
-}
+    // Edad
+    if(Age.value === "") {
+        showError(Age, "Se requiere introducir Edad");
+    }else if(Age.value < 0 || Age.value > 999) {
+        showError(Age, "La edad no puede ser menor a 0 o mayor a 999")
+    } else {
+        showSuccess(Age); 
+    }
 
-// Url
-if(url.value === "") {
-    showError(url, "Se requiere introducir url");
-}else if(isURLValid(url.value)) {
-    showError(url, "La url no es valida")
-} else {
-    showSuccess(url); 
-}
+    // Url
+    if(url.value === "") {
+        showError(url, "Se requiere introducir url");
+    }else if(isURLValid(url.value)) {
+        showError(url, "La url no es valida")
+    } else {
+        showSuccess(url); 
+    }
 
-// Email
-if(email.value === "") {
-    showError(email, "Se requiere introducir email");
-}else if(!isValidEmail(email.value)) {
-    showError(email, "El email no es valido")
-} else {
-    showSuccess(email); 
-}
+    // Email
+    if(email.value === "") {
+        showError(email, "Se requiere introducir email");
+    }else if(!isValidEmail(email.value)) {
+        showError(email, "El email no es valido")
+    } else {
+        showSuccess(email); 
+    }
 
-// Contraseña
-if(password.value === "") {
-    showError(password, "Se requiere introducir contraseña");
-}else if(cuentaContraseña(password.value) < 8 ) {
-    showError(password, "La contraseña debe contener al menos 8 caracteres")
-} else {
-    showSuccess(password); 
-}
+    // Contraseña
+    if(password.value === "") {
+        showError(password, "Se requiere introducir contraseña");
+    }else if(cuentaContraseña(password.value) < 8 ) {
+        showError(password, "La contraseña debe contener al menos 8 caracteres")
+    } else {
+        showSuccess(password); 
+    }
 
-// Confirmación de contraseña
-if(password2.value === "") {
-    showError(password2, "Se requiere introducir de nuevo la contraseña");
-}else if(password2.value !== password.value) {
-    showError(password2, "La contraseña no coincide")
-} else {
-    showSuccess(password2); 
-}
-
-
+    // Confirmación de contraseña
+    if(password2.value === "") {
+        showError(password2, "Se requiere introducir de nuevo la contraseña");
+    }else if(password2.value !== password.value) {
+        showError(password2, "La contraseña no coincide")
+    } else {
+        showSuccess(password2); 
+    };
 });
