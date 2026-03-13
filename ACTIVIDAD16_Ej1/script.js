7a2f440497a66a11dac0b0eae88b921f89fb9ad2
Shuuunt <carlosevinah@gmail.com>
2026-03-13 16:38:47 +0100

    Primeras funciones

--- ACTIVIDAD16_Ej1/script.js
+++ ACTIVIDAD16_Ej1/script.js
@@ -5,3 +5,28 @@ const url = document.getElementById("url");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+
+function showError(input, message) {
+    const fromControl = input.parentElement;
+    fromControl.className = "form-control error";
+
+    const small = fromControl.querySelector("small");
+    small.innerText = message;
+}
+
+function showSuccess(input) {
+    const fromControl = input.parentElement;
+    fromControl.className = "form-control success";
+}
+
+
+
+form.addEventListener("submit", function(e) {
+e.preventDefault();
+
+if(username.value === "") {
+    showError(username, "Se requiere introducir usuario");
+} else {
+    showSuccess(username); 
+}
+});
