39099ad10e6025f4f99ec5c26f09eff6b2d8185b
Shuuunt <carlosevinah@gmail.com>
2026-03-13 23:48:40 +0100

    EJ2ErrorFunc

--- ACTIVIDAD16_Ej2/script.js
+++ ACTIVIDAD16_Ej2/script.js
@@ -8,6 +8,8 @@ const LogoState = document.getElementById("Imagen")
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
+// Funciones
+
 // hacer exchange para los rates y actualizar el DOM
 function calculate() {
     const currencySym_one = currencyEl_one.value;
@@ -16,20 +18,45 @@ function calculate() {
     LogoState.className = "Imagen giro";
     Estado.innerText = "Cargando 🕧"
 
-    fetch(`https://v6.exchangerate-api.com/v6/fdbc5881e41d95f8cb9bf865/latest/${currencySym_one}`)
-        .then(res => res.json())
-        .then(data => {
-        // console.log(data)
-
+    if(AmountEl_one.value < 0) {
+        showError(AmountEl_one, "Numeros negativos no admitidos");
         LogoState.className = "";
         Estado.innerText = "Cargado 👍"
+    } else {
+        showSuccess(AmountEl_one);
 
-        const rate = data.conversion_rates[currencySym_two];
+        fetch(`https://v6.exchangerate-api.com/v6/fdbc5881e41d95f8cb9bf865/latest/${currencySym_one}`)
+            .then(res => res.json())
+            .then(data => {
+            // console.log(data)
 
-        rateEl.innerHTML = `1 ${currencySym_one} = ${rate} ${currencySym_two}`
-        AmountEl_two.value = (AmountEl_one.value * rate).toFixed(2);
-    })
+            LogoState.className = "";
+            Estado.innerText = "Cargado 👍"
+
+            const rate = data.conversion_rates[currencySym_two];
+
+            rateEl.innerHTML = `1 ${currencySym_one} = ${rate} ${currencySym_two}`
+            AmountEl_two.value = (AmountEl_one.value * rate).toFixed(2);
+        })
+    }
+
 };
+
+// error
+function showError(input, message) {
+    AmountEl_one.value = 0;
+    const fromControl = input.parentElement;
+    fromControl.className = "currency error";
+
+    const small = fromControl.querySelector("small");
+    small.innerText = message;
+};
+
+//Exito
+function showSuccess(input) {
+    const fromControl = input.parentElement;
+    fromControl.className = "currency success"
+}
 
 
 // Events
