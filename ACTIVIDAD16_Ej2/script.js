778782122baecf3868c403beead1abccbf5a8785
Shuuunt <carlosevinah@gmail.com>
2026-03-13 23:28:57 +0100

    FuncLoad

--- ACTIVIDAD16_Ej2/script.js
+++ ACTIVIDAD16_Ej2/script.js
@@ -2,6 +2,8 @@ const currencyEl_one = document.getElementById("currency-one");
 const AmountEl_one = document.getElementById("amount-one");
 const currencyEl_two = document.getElementById("currency-two");
 const AmountEl_two = document.getElementById("amount-two");
+const Estado = document.getElementById("cargando")
+const LogoState = document.getElementById("Imagen")
 
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
@@ -11,10 +13,16 @@ function calculate() {
     const currencySym_one = currencyEl_one.value;
     const currencySym_two = currencyEl_two.value;
 
+    LogoState.className = "Imagen giro";
+    Estado.innerText = "Cargando 🕧"
+
     fetch(`https://v6.exchangerate-api.com/v6/fdbc5881e41d95f8cb9bf865/latest/${currencySym_one}`)
         .then(res => res.json())
         .then(data => {
         // console.log(data)
+
+        LogoState.className = "";
+        Estado.innerText = "Cargado 👍"
 
         const rate = data.conversion_rates[currencySym_two];
 
